cadea86a859259c7741b88352c1c730c70c1730f
Cipher27 <26444048+Cipher27@users.noreply.github.com>
2025-08-10 09:44:52 -0400

    Update maven URL
    Signed-off-by: Cipher27 <26444048+Cipher27@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
     mavenLocal()
     mavenCentral()
     maven {
-        setUrl("https://nexus.botwithus.net/repository/maven-snapshots/")
+        setUrl("https://nexus.botwithus.net/repository/maven-releases/")
     }
 }
 
